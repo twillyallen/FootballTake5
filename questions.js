842177e25629710d8306e186adb6a1235425d43b
Thomas Allen <thomas.allen5@snhu.edu>
2025-09-13 10:00:01 -0700

    New Qs

--- questions.js
+++ questions.js
@@ -23,11 +23,11 @@ export const CALENDAR = {
     { question: "DPOY 2023?", choices: ["Bosa", "Watt", "Garrett", "Parsons"], answer: 2 }
   ],
   "2025-09-13": [
-    { question: "Name one of the 3 players who are 6'5\" and ran a 4.3", choices: ["Davante Adams", "Donte Thornton", "Don Hutson", "Courtland Sutton"], answer: 1 },
-    { question: "Who was called 'Mr Butt Fumble'?", choices: ["Kurt Warner", "Derek Carr", "Tim Tebow", "Mark Sanchez"], answer: 3 },
-    { question: "How many teams has Peyton Manning played for?", choices: ["1", "3", "2", "1"], answer: 2 },
-    { question: "Which team was Drew Brees drafted by?", choices: ["Chargers", "Saints", "Bengals", "49ers"], answer: 0 },
-    { question: "DPOY 2023?", choices: ["Bosa", "Watt", "Garrett", "Parsons"], answer: 2 }
+    { question: "Who is 2nd All-Time in Receiving Yards after turning the age of 40?", choices: ["Jerry Rice", "Tom Brady", "Aaron Rodgers", "Steve Smith Sr."], answer: 1 },
+    { question: "Who won Super Bowl XXXI MVP?", choices: ["Kurt Warner", "Terrell Davis", "Desmond Howard", "Steve Young"], answer: 2 },
+    { question: "Which team drafted Eli Manning?", choices: ["Colts", "Giants", "Chargers", "Broncos"], answer: 2 },
+    { question: "Which franchise was *formerly* a part of the AFC West?", choices: ["Seahawks", "Chiefs", "Rams", "Raiders"], answer: 0 },
+    { question: "How many career TD Passes did Dan Marino finish with?", choices: ["487", "392", "420", "438"], answer: 2 }
   ]
 };
 
